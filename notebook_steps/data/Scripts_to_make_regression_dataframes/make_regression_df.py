9d59d2b4f382eb9b567a7e193110ea7b6c77bfc1
nanel96 <nanel96@yahoo.com>
2021-05-29 19:49:48 -0600

    run regressions

--- notebook_steps/data/Scripts_to_make_regression_dataframes/make_regression_df.py
+++ notebook_steps/data/Scripts_to_make_regression_dataframes/make_regression_df.py
@@ -4,38 +4,36 @@ import os, sys
 
 currentdir = os.path.dirname(os.path.realpath('make_regression_df.py'))
 parentdir = os.path.dirname(currentdir)
-print(parentdir)
 parentdir = os.path.dirname(parentdir)
-print(parentdir)
 sys.path.append(parentdir)
 import Delta_Correlation as dc
 import statsmodels.stats.multitest as ssm
 
-input_cancer_type = sys.argv[1]
+def compute_regression(input_cancer_type):
 
-if input_cancer_type == "CCRCC":
-    cancer = cptac.Ccrcc()
-elif input_cancer_type == "Endometrial":
-    cancer = cptac.Endometrial()
-elif input_cancer_type == "LUAD":
-    cancer = cptac.Luad()
-elif input_cancer_type == "HNSCC":
-    cancer  = cptac.Hnscc()
-elif input_cancer_type == "LSCC":
-    cancer = cptac.Lscc()
-elif input_cancer_type == "PDAC":
-    cancer = cptac.Pdac()
+    if input_cancer_type == "CCRCC":
+        cancer = cptac.Ccrcc()
+    elif input_cancer_type == "Endometrial":
+        cancer = cptac.Endometrial()
+    elif input_cancer_type == "LUAD":
+        cancer = cptac.Luad()
+    elif input_cancer_type == "HNSCC":
+        cancer  = cptac.Hnscc()
+    elif input_cancer_type == "LSCC":
+        cancer = cptac.Lscc()
+    elif input_cancer_type == "PDAC":
+        cancer = cptac.Pdac()
 
-df = dc.get_prot_trans_df(cancer)
-results = df.groupby('Gene').apply(dc.regression)
-reg_df = pd.DataFrame(list(results))
-reg_df.index = results.index
-reg_df.reset_index(inplace = True)
-reg_df = reg_df.dropna()
-reg_df['interaction_FDR'] = ssm.fdrcorrection(reg_df['interaction_pval'])[1]
-reg_df['condition_FDR'] = ssm.fdrcorrection(reg_df['condition_pval'])[1]
-reg_df['intercept_FDR'] = ssm.fdrcorrection(reg_df['intercept_pval'])[1]
-reg_df['Cancer'] = [input_cancer_type] * len(reg_df)
+    df = dc.get_prot_trans_df(cancer)
+    results = df.groupby('Gene').apply(dc.regression)
+    reg_df = pd.DataFrame(list(results))
+    reg_df.index = results.index
+    reg_df.reset_index(inplace = True)
+    reg_df = reg_df.dropna()
+    reg_df['interaction_FDR'] = ssm.fdrcorrection(reg_df['interaction_pval'])[1]
+    reg_df['condition_FDR'] = ssm.fdrcorrection(reg_df['condition_pval'])[1]
+    reg_df['intercept_FDR'] = ssm.fdrcorrection(reg_df['intercept_pval'])[1]
+    reg_df['Cancer'] = [input_cancer_type] * len(reg_df)
 
-file_name = input_cancer_type + '_regressions.csv'
-reg_df.to_csv(file_name, index = False)
+    file_name = input_cancer_type + '_regressions.csv'
+    reg_df.to_csv(file_name, index = False)
